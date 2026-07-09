77f42ee5bdbbf40c302d684fe2d1f12bdead3813
JRK <jrk@MacBook-Pro-JRK.local>
2026-07-09 14:27:50 +0300

    Amplify spiral perspective

--- placeholder.js
+++ placeholder.js
@@ -146,7 +146,7 @@ const setBeat = (value) => {
   root.style.setProperty("--beat", value.toFixed(3));
 };
 
-const getScale = (t) => 1.82 - t * 1.62;
+const getScale = (t) => 2.35 - t * 2.13;
 const phrase = "танцуй ";
 
 const getFontSize = () => {
